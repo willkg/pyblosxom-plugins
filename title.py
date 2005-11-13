f8b63456c4b41e83c681b55bd39a0386844b4e1d
willg <willg@7dd3543b-5105-0410-a0b9-eaf5b54a3dfd>
2005-11-13 18:48:47 +0000

    Placed the title plugin in the Public Domain.
    git-svn-id: file:///Users/willg/tmp/pyblplugins/svn/pyblplugins/trunk@14 7dd3543b-5105-0410-a0b9-eaf5b54a3dfd

--- title.py
+++ title.py
@@ -10,31 +10,13 @@ NOTE: This plugin is not required if you're using PyBlosxom 1.3 and
 later.
 
 
-Permission is hereby granted, free of charge, to any person
-obtaining a copy of this software and associated documentation
-files (the "Software"), to deal in the Software without restriction,
-including without limitation the rights to use, copy, modify,
-merge, publish, distribute, sublicense, and/or sell copies of the
-Software, and to permit persons to whom the Software is furnished
-to do so, subject to the following conditions:
+This plugin is placed in the Public Domain.
 
-The above copyright notice and this permission notice shall be
-included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
-BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
-ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-
-Copyright 2002-2005 Will Guaraldi
 
 SUBVERSION VERSION: $Id$
 
 Revisions:
+2005-11-13 - Place into the Public Domain.
 2005-11-11 - Pulled into new VCS.
 1.3 - (26 October, 2005) pulled into new VCS
 1.0 - created
