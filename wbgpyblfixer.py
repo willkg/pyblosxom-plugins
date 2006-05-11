0458688dfe8d9f91607b1c411e92368224196fb4
willg <willg@7dd3543b-5105-0410-a0b9-eaf5b54a3dfd>
2006-05-11 19:06:49 +0000

    Wrote a quick plugin to help those of us who have trouble spelling PyBlxosxoxsom.
    git-svn-id: file:///Users/willg/tmp/pyblplugins/svn/pyblplugins/trunk@26 7dd3543b-5105-0410-a0b9-eaf5b54a3dfd

---
+++ wbgpyblfixer.py
@@ -0,0 +1,53 @@
+"""
+Plugin to help those of us who can never spell PyBloxosom right.
+
+
+This code is placed in the public domain--use it as you so desire.
+
+SUBVERSION VERSION: $Id$
+
+Revisions:
+2006-05-11 - First writing to help Carol.
+"""
+import re
+
+REGEXP = re.compile(r'p[^x\s]+x[^m\s]+m', re.I)
+
+def good_character(c):
+   return c.isspace() or c == "."
+
+def filter_text(body):
+   r = REGEXP.search(body)
+   while r:
+      if r.start() == 0 or good_character(r.string[r.start() - 1]):
+         if r.end() == len(r.string) or good_character(r.string[r.end()]):
+            body = body[:r.start()] + "PyBlosxom" + r.string[r.end():]
+      r = REGEXP.search(body, r.end())
+   return body
+
+def cb_story(args):
+   entry = args["entry"]
+   body = entry["body"]
+
+   entry["body"] = filter_text(body)
+
+   return args
+
+
+# this is for testing to make sure I got the filter_test function
+# right.
+if __name__ == "__main__":
+   def run_test(item):
+      print "'" + item + "'", filter_text(item)
+
+   run_test("pyblosxom")
+   run_test(" pyblosxom")
+   run_test("pyblosxom ")
+   run_test("pyblosxomm")
+   run_test("dpyblosxom")
+   run_test("dpyblosxom pyblosxom")
+   run_test("pyblosxom m pyblosxom")
+   run_test("pyblosxom m pdyblosxom")
+   run_test("still using PyBlosxom to")
+   run_test("track PyBlosxom down PyBlosxom issues")
+   run_test("track PBosxom down PyBlosxom issues")
